5bcd9687077680580e1724fc8420f99a1afa0a20
dnava <d.navarro03@ufromail.cl>
2022-09-26 20:57:18 -0300

    commit Diego1

---
+++ src/main/java/Arbol.java
@@ -0,0 +1,31 @@
+public class Arbol {
+    String nombre;
+    int antiguedad;
+    int altura;
+    String frutas;
+    String colorDeHojas;
+
+    public void generarOxigeno(){
+
+    }
+
+    public void absorberDióxidoDeCarbono(){
+
+    }
+
+    public void absorberLuz(){
+
+    }
+
+    public void absorberAgua(){
+
+    }
+
+    public void Fotosíntesis(){
+
+    }
+
+    public void Crecer(){
+
+    }
+}
